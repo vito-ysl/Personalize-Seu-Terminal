c071d96f71b97f26254a3680ea10fb3ba625eeda
vito-ysl <vitorevangelista0557@gmail.com>
2025-05-05 14:39:33 -0300

    persolizar: 05/05/25 - 14:39

--- script.js
+++ script.js
@@ -3,3 +3,20 @@
 window.onload = function() {
     alert("Bem-vindo à documentação do Terminal Linux!");
 };
+
+document.addEventListener('DOMContentLoaded', () => {
+    const copyButtons = document.querySelectorAll('.copy-button');
+    
+    copyButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const commandText = button.nextElementSibling.textContent;
+            navigator.clipboard.writeText(commandText).then(() => {
+                const originalText = button.textContent;
+                button.textContent = 'Copiado!';
+                setTimeout(() => {
+                    button.textContent = originalText;
+                }, 2000);
+            });
+        });
+    });
+});
